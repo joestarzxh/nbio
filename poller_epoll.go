5ac79b8f03402319f44d345ce5ed87d0686af845
lesismal <wuqiongjishu@163.com>
2021-05-14 16:54:06 +0800

    use std listener

--- poller_epoll.go
+++ poller_epoll.go
@@ -7,6 +7,7 @@
 package nbio
 
 import (
+	"errors"
 	"io"
 	"net"
 	"runtime"
@@ -280,4 +281,42 @@ func newPoller(g *Gopher, isListener bool, index int) (*poller, error) {
 	}
 
 	return p, nil
+}
+
+func dupStdConn(conn net.Conn) (*Conn, error) {
+	sc, ok := conn.(interface {
+		SyscallConn() (syscall.RawConn, error)
+	})
+	if !ok {
+		return nil, errors.New("RawConn Unsupported")
+	}
+	rc, err := sc.SyscallConn()
+	if err != nil {
+		return nil, errors.New("RawConn Unsupported")
+	}
+
+	var newFd int
+	errCtrl := rc.Control(func(fd uintptr) {
+		newFd, err = syscall.Dup(int(fd))
+	})
+
+	if errCtrl != nil {
+		return nil, errCtrl
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = syscall.SetNonblock(newFd, true)
+	if err != nil {
+		syscall.Close(newFd)
+		return nil, err
+	}
+
+	return &Conn{
+		fd:    newFd,
+		lAddr: conn.LocalAddr(),
+		rAddr: conn.RemoteAddr(),
+	}, nil
 }
